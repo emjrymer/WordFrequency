39db07bc733c48d23798aff747aeae33082cd5a5
emjwivell <emjwivell@gmail.com>
2016-02-04 08:50:13 -0500

    reverse sort added and top twenty added

--- WordId.py
+++ WordId.py
@@ -4,8 +4,8 @@ with open("sample.txt") as my_file:
 answer = input("Do you want to enter a sentence or run provided text? ")
 
 if answer == "yes":
-    given_sentence = input("What is your sentence? ")#"This this zebra zebra is zebra ball ace ace ace that this wonderful."
-    new_words = given_sentence.lower().replace(".", "").split()
+    given_sentence = input("What is your sentence? ")
+    new_words = given_sentence.lower().replace(".", "").replace("'", "").replace("--", "").replace("/n", "").replace("-", "").replace(",", "").replace("!", "").replace("?", "").split()
 else:
     new_words = contents.lower().replace(".", "").split()
 
@@ -13,15 +13,15 @@ d = {}
 
 for word in set(new_words):
     d[word] = new_words.count(word)
-    #print(word, new_words.count(word)) #don't need to print this list or it will take forever
 
 l = d.items()
 
 def val_sort(val):
     return val[1]
 
-d = sorted(l, key=val_sort)
+d = sorted(l, key=val_sort, reverse = True)
+
 print("Word Frequency: " + str(d))
 
-top_twenty = (d[-5:])
-print("Twenty most popular: " + str(top_twenty))
+for _ in d[:20]:
+    print(_)
